911e32c7dd1a5fc79bad149a460582ff78a81042
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-10-09 06:25:53 +0900

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
         name='wcpan.acd',
-        version='1.0.0.dev6',
+        version='1.0.0.dev7',
         author='Wei-Cheng Pan',
         author_email='legnaleurc@gmail.com',
         url='https://github.com/legnaleurc/wcpan.acd',
