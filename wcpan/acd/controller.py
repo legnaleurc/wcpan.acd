d567f0a0c5ce46a9386f56dceb8b562555e38de0
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-02-22 03:50:05 +0800

    refactor

--- wcpan/acd/controller.py
+++ wcpan/acd/controller.py
@@ -1,6 +1,6 @@
-from functools import partial as ftp
+import functools as ft
 import hashlib
-import multiprocessing as mp
+import threading
 import time
 from typing import Any, Awaitable, Dict, Generator, List, Optional
 from typing.io import IO as Stream
@@ -135,22 +135,22 @@ class ACDClientController(object):
     async def create_directory(self, node: Node,
                                name: str) -> Awaitable[Dict[str, Any]]:
         await self._ensure_alive()
-        fn = ftp(self._link.create_folder, name, node.id)
+        fn = ft.partial(self._link.create_folder, name, node.id)
         rv = await self._worker.do(fn)
         return rv
 
     async def download_node(self, node: Node,
                             local_path: str) -> Awaitable[MD5Hash]:
         await self._ensure_alive()
-        fn = ftp(self._download, node, local_path)
+        fn = ft.partial(self._download, node, local_path)
         rv = await self._worker.do(fn)
         return rv
 
     async def get_changes(self, checkpoint: CheckPoint,
                           include_purged: bool) -> Awaitable[Stream]:
         await self._ensure_alive()
-        fn = ftp(self._link.get_changes, checkpoint=checkpoint,
-                 include_purged=include_purged, silent=True, file=None)
+        fn = ft.partial(self._link.get_changes, checkpoint=checkpoint,
+                        include_purged=include_purged, silent=True, file=None)
         rv = await self._worker.do(fn)
         return rv
 
@@ -162,14 +162,14 @@ class ACDClientController(object):
 
     async def move_to_trash(self, node_id: NodeID) -> Awaitable[Dict[str, Any]]:
         await self._ensure_alive()
-        fn = ftp(self._link.move_to_trash, node_id)
+        fn = ft.partial(self._link.move_to_trash, node_id)
         rv = await self._worker.do(fn)
         return rv
 
     async def upload_file(self, node: Node,
                           local_path: str) -> Awaitable[Dict[str, Any]]:
         await self._ensure_alive()
-        fn = ftp(self._link.upload_file, str(local_path), node.id)
+        fn = ft.partial(self._link.upload_file, str(local_path), node.id)
         rv = await self._worker.do(fn)
         return rv
 
@@ -192,133 +192,15 @@ class ACDClientController(object):
         return hasher.hexdigest()
 
 
+def off_main_thread(afn):
+    @ft.wraps(afn)
+    async def run_in_pool(self, *args, **kwargs):
+        fn = ft.partial(afn, self, *args, **kwargs)
+        return await self._pool.do(fn)
+    return run_in_pool
+
+
 class ACDDBController(object):
-
-    def __init__(self, auth_path: str) -> None:
-        # acd_cli uses thread local storage to isolate sqlite3 connection for
-        # each thread, so it would be better to have individual database object
-        # in each worker
-        self._pool = DatabaseWorkerPool(auth_path)
-
-    def close(self) -> None:
-        self._pool.close()
-
-    async def resolve_path(self, remote_path: str) -> Awaitable[Optional[Node]]:
-        async with self._pool.raii() as db:
-            rv = await db.resolve_path(remote_path)
-            return rv
-
-    async def get_child(self, node: Node,
-                        name: str) -> Awaitable[Optional[Node]]:
-        async with self._pool.raii() as db:
-            rv = await db.get_child(node, name)
-            return rv
-
-    async def get_children(self, node: Node) -> Awaitable[List[Node]]:
-        async with self._pool.raii() as db:
-            rv = await db.get_children(node)
-            return rv
-
-    async def get_path(self, node: Node) -> Awaitable[str]:
-        async with self._pool.raii() as db:
-            rv = await db.get_path(node)
-            return rv
-
-    async def get_node(self, node_id: NodeID) -> Awaitable[Optional[Node]]:
-        async with self._pool.raii() as db:
-            rv = await db.get_node(node_id)
-            return rv
-
-    async def find_by_regex(self, pattern: str) -> Awaitable[List[Node]]:
-        async with self._pool.raii() as db:
-            rv = await db.find_by_regex(pattern)
-            return rv
-
-    async def get_checkpoint(self) -> Awaitable[CheckPoint]:
-        async with self._pool.raii() as db:
-            rv = await db.get_checkpoint()
-            return rv
-
-    async def reset(self) -> Awaitable[None]:
-        async with self._pool.raii() as db:
-            await db.reset()
-
-    async def remove_purged(self, nodes: List[NodeID]) -> Awaitable[None]:
-        async with self._pool.raii() as db:
-            await db.remove_purged(nodes)
-
-    async def insert_nodes(self, nodes: List[Dict[str, Any]],
-                           partial: bool = True) -> Awaitable[None]:
-        async with self._pool.raii() as db:
-            await db.insert_nodes(nodes, partial)
-
-    async def update_last_sync_time(self) -> Awaitable[None]:
-        async with self._pool.raii() as db:
-            await db.update_last_sync_time()
-
-    async def update_check_point(self,
-                                 check_point: CheckPoint) -> Awaitable[None]:
-        async with self._pool.raii() as db:
-            await db.update_check_point(check_point)
-
-
-class DatabaseWorkerPool(object):
-
-    def __init__(self, auth_path: str) -> None:
-        self._auth_path = auth_path
-        self._idle = []
-        self._busy = set()
-        self._max = mp.cpu_count()
-        self._lock = tl.Condition()
-
-    def close(self) -> None:
-        for worker in self._idle:
-            worker.close()
-        for worker in self._busy:
-            worker.close()
-
-    async def get_worker(self) -> Awaitable['DatabaseWorker']:
-        while True:
-            worker = self._try_get_or_create()
-            if worker:
-                self._busy.add(worker)
-                return worker
-
-            # no worker available, wait for idle
-            await self._lock.wait()
-
-    def recycle(self, worker: 'DatabaseWorker') -> None:
-        self._busy.remove(worker)
-        self._idle.append(worker)
-        self._lock.notify()
-
-    def raii(self) -> 'DatabaseWorkerRecycler':
-        return DatabaseWorkerRecycler(self)
-
-    def _try_get_or_create(self) -> Optional['DatabaseWorker']:
-        worker = None
-        if self._idle:
-            worker = self._idle.pop(0)
-        elif len(self._busy) < self._max:
-            worker = DatabaseWorker(self._auth_path)
-        return worker
-
-
-class DatabaseWorkerRecycler(object):
-
-    def __init__(self, pool: DatabaseWorkerPool) -> None:
-        self._pool = pool
-        self._worker = None
-
-    async def __aenter__(self) -> Awaitable['DatabaseWorker']:
-        self._worker = await self._pool.get_worker()
-        return self._worker
-
-    async def __aexit__(self, *args, **kwargs) -> Awaitable[None]:
-        self._pool.recycle(self._worker)
-
-
-class DatabaseWorker(object):
 
     # magic strings from acd_cli
     _CHECKPOINT_KEY = 'checkpoint'
@@ -326,95 +208,91 @@ class DatabaseWorker(object):
 
     def __init__(self, auth_path: str) -> None:
         self._auth_path = auth_path
-        self._worker = ww.AsyncWorker()
-        # not only for lazy initialization, but also preventing thread local
-        # storage mess, i.e. ONLY ININTIALIZE DATABASE IN THE WORKER THREAD!
-        # and all operation MUST be done in the worker thread as well.
-        self._db = None
+        self._pool = ww.AsyncWorkerPool()
+        # acd_cli uses thread local storage to isolate sqlite3 connection for
+        # each thread, so it would be better to have individual database object
+        # in each worker
+        self._tls = threading.local()
 
     def close(self) -> None:
-        self._worker.stop()
-        self._db = None
+        self._pool.stop()
+        # TODO cleanup for each worker
 
+    @off_main_thread
     async def resolve_path(self, remote_path: str) -> Awaitable[Optional[Node]]:
-        await self._ensure_alive()
-        fn = ftp(self._db.resolve, remote_path)
-        rv = await self._worker.do(fn)
+        rv = self._db.resolve(remote_path)
         return rv
 
+    @off_main_thread
     async def get_child(self, node: Node,
                         name: str) -> Awaitable[Optional[Node]]:
-        await self._ensure_alive()
-        fn = ftp(self._db.get_child, node.id, name)
-        child_node = await self._worker.do(fn)
+        child_node = self._db.get_child(node.id, name)
         return child_node
 
+    @off_main_thread
     async def get_children(self, node: Node) -> Awaitable[List[Node]]:
-        await self._ensure_alive()
-        fn = ftp(self._db.list_children, node.id)
-        folders, files = await self._worker.do(fn)
+        folders, files = self._db.list_children(node.id)
         children = folders + files
         return children
 
+    @off_main_thread
     async def get_path(self, node: Node) -> Awaitable[str]:
-        await self._ensure_alive()
-        fn = ftp(self._db.first_path, node.id)
-        dirname = await self._worker.do(fn)
+        dirname = self._db.first_path(node.id)
         return dirname + node.name
 
+    @off_main_thread
     async def get_node(self, node_id: NodeID) -> Awaitable[Optional[Node]]:
-        await self._ensure_alive()
-        fn = ftp(self._db.get_node, node_id)
-        rv = await self._worker.do(fn)
+        rv = self._db.get_node(node_id)
         return rv
 
+    @off_main_thread
     async def find_by_regex(self, pattern: str) -> Awaitable[List[Node]]:
-        await self._ensure_alive()
-        fn = ftp(self._db.find_by_regex, pattern)
-        rv = await self._worker.do(fn)
+        rv = self._db.find_by_regex(pattern)
         return rv
 
+    @off_main_thread
     async def get_checkpoint(self) -> Awaitable[CheckPoint]:
-        await self._ensure_alive()
-        fn = ftp(self._db.KeyValueStorage.get, self._CHECKPOINT_KEY)
-        rv = await self._worker.do(fn)
+        rv = self._db.KeyValueStorage.get(self._CHECKPOINT_KEY)
         return rv
 
+    @off_main_thread
     async def reset(self) -> Awaitable[None]:
-        await self._ensure_alive()
-        await self._worker.do(self._db.drop_all)
-        await self._worker.do(self._db.init)
+        self._db.drop_all()
+        self._db.init()
 
+    @off_main_thread
     async def remove_purged(self, nodes: List[NodeID]) -> Awaitable[None]:
-        await self._ensure_alive()
-        fn = ftp(self._db.remove_purged, nodes)
-        await self._worker.do(fn)
+        self._db.remove_purged(nodes)
 
+    @off_main_thread
     async def insert_nodes(self, nodes: List[Dict[str, Any]],
                            partial: bool = True) -> Awaitable[None]:
-        await self._ensure_alive()
-        fn = ftp(self._db.insert_nodes, nodes, partial=partial)
-        await self._worker.do(fn)
+        self._db.insert_nodes(nodes, partial=partial)
 
+    @off_main_thread
     async def update_last_sync_time(self) -> Awaitable[None]:
-        await self._ensure_alive()
-        fn = ftp(self._db.KeyValueStorage.update, {
+        self._db.KeyValueStorage.update({
             self._LAST_SYNC_KEY: time.time(),
         })
-        await self._worker.do(fn)
 
+    @off_main_thread
     async def update_check_point(self,
                                  check_point: CheckPoint) -> Awaitable[None]:
-        fn = ftp(self._db.KeyValueStorage.update, {
+        self._db.KeyValueStorage.update({
             self._CHECKPOINT_KEY: check_point,
         })
-        await self._worker.do(fn)
 
-    async def _ensure_alive(self) -> Awaitable[None]:
-        if not self._db:
-            self._worker.start()
-            await self._worker.do(self._create_db)
+    @property
+    def _db(self):
+        assert not is_main_thread()
+        db = getattr(self._tls, 'db', None)
+        if db is None:
+            db = DB.NodeCache(self._auth_path)
+            setattr(self._tls, 'db', db)
+        return db
 
-    def _create_db(self) -> None:
-        assert self._db is None
-        self._db = DB.NodeCache(self._auth_path)
+
+def is_main_thread():
+    mt = threading.main_thread()
+    ct = threading.current_thread()
+    return mt.ident == ct.ident
